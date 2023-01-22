12a78452ec5c4ec07063a82900108bc5e39e18b1
SandyLOC <sandrapl.18@gmail.com>
2023-01-22 15:12:45 -0800

    node packages added and local conection to mongo set

--- routes/app.js
+++ routes/app.js
@@ -1,3 +1,10 @@
+// Gets access to environment variables/settings
+require("dotenv").config();
+
+// Connects to the database
+require("../db");
+
+// Express handles http requests
 const express = require("express");
 
 const app = express();
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const app = require("./routes/app");
 
 const PORT = process.env.PORT || 3000;
 
-
-
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
